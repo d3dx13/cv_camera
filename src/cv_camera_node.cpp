7099cd933b83fe5951bc6c8044d5c779f471caf3
d3dx13 <36724128+d3dx13@users.noreply.github.com>
2022-04-13 22:18:22 +0300

    Update cv_camera_node.cpp

--- src/cv_camera_node.cpp
+++ src/cv_camera_node.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "camera");
+  ros::init(argc, argv, "camera", ros::init_options::AnonymousName);
   ros::NodeHandle private_node("~");
   cv_camera::Driver driver(private_node, private_node);
 
